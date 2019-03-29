43615a72ee3053cc1d080b6f17b07a55c880395d
Itachi6912110 <itachi691211011129@gmail.com>
2019-03-29 21:22:34 +0800

    code finish

---
+++ Lab2/1_prod_con.py
@@ -0,0 +1,31 @@
+n_item = 0
+
+def Producer(n):
+    global n_item
+    for i in range(n):
+        #assert 0
+        yield
+        yield
+        yield
+        print("Put an item")
+        n_item += 1
+
+def Consumer(n):
+    global n_item
+    n_get = 0
+    while n_get < n:
+        #assert 0
+        yield
+        yield
+        if n_item > 0:
+            print("Get an item")
+            n_item -= 1
+            n_get += 1
+
+def main_loop(threads):
+    from itertools import zip_longest
+    for dummy in zip_longest(*threads):
+        print("=== clk ===")
+
+main_loop([Producer(10), Consumer(10)])
+#main_loop([Producer(10)])
---
+++ Lab2/2_2_event_fifo.py
@@ -0,0 +1,70 @@
+n_item = 0
+INIT_EVENT = 5
+CLOCK_EVENT = 7
+WRITE_EVENT = 11
+
+def Clk(n):
+    yield INIT_EVENT
+    for i in range(n):
+        # TODO
+        #pass
+        TriggerEvent(CLOCK_EVENT)
+        yield CLOCK_EVENT
+
+def Producer(n):
+    global n_item
+    for i in range(n):
+        yield CLOCK_EVENT
+        yield CLOCK_EVENT
+        yield CLOCK_EVENT
+        n_item += 1
+        print("Put an item")
+        TriggerEvent(WRITE_EVENT)
+
+def GetItem():
+    # TODO
+    #pass
+    global n_item
+    while True:
+        yield CLOCK_EVENT       
+        if n_item == 0:
+            yield WRITE_EVENT
+            print("Get an item")
+        n_item -= 1
+
+def Consumer(n):
+    n_get = 0
+    while n_get < n:
+        yield from GetItem()
+        n_get += 1
+
+from collections import deque
+event_pending_on_list = [list() for _ in range(100)]
+event_pending = deque()
+event_pending.append(INIT_EVENT)
+
+def TriggerEvent(idx):
+    # TODO
+    #pass
+    event_pending.append(idx)
+
+def WaitOnNextEvent(t):
+    try:
+        waiting_on = next(t)
+        event_pending_on_list[waiting_on].append(t)
+    except StopIteration:
+        pass
+
+def main_loop(threads):
+    for t in threads:
+        WaitOnNextEvent(t)
+    # while what?
+    while event_pending:
+        # TODO
+        trigger = event_pending.popleft()
+        print("handling {}".format(trigger))
+        handling, event_pending_on_list[trigger] = event_pending_on_list[trigger], list()
+        for t in handling:
+            WaitOnNextEvent(t)
+
+main_loop([Clk(100), Producer(10), Consumer(10)])
